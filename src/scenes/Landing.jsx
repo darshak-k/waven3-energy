531bbc1445622a891043bc961d13558eb95833d2
Darshak Kachchhi <87572369+darshak-k@users.noreply.github.com>
2024-01-29 23:09:24 -0500

    Initial Code

---
+++ src/scenes/Landing.jsx
@@ -0,0 +1,88 @@
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import SwingLogo from "../assets/SwingLogo.png";
+
+const Landing = ({ setSelectedPage }) => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+    >
+      {/* IMAGE SECTION */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1 }}
+        variants={{
+          hidden: { opacity: 0, x: 70, y: 70 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2"
+      >
+        {isAboveLarge ? (
+          <img
+            alt="profile"
+            className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src={SwingLogo}
+          />
+        ) : (
+          <img
+            alt="profile"
+            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
+            src={SwingLogo}
+          />
+        )}
+      </motion.div>
+
+      {/* MAIN TEXT */}
+      <div className="z-30 basis-3/5 mt-12 md:mt-32">
+        {/* HEADINGS */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            JOIN US IN CHANGING THE WORLD TODAY
+          </p>
+
+          <p className="mt-10 mb-7 text-4xl text-center md:text-start">
+            WAVEN 3 TECH
+          </p>
+        </motion.div>
+
+        {/* CALL TO ACTIONS */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <AnchorLink
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-blue hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
+            ENTER
+          </AnchorLink>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
