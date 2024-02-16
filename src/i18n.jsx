bd7541d08c4bfe01c27e56c000115e405eeff4f8
Darshak Kachchhi <87572369+darshak-k@users.noreply.github.com>
2024-02-16 18:09:46 -0500

    Fixed language backend error

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,20 +1,25 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import i18nBackend from "i18next-http-backend";
-
-const getCurrentHost = "http://localhost:3000";
+import resourcesToBackend from "i18next-resources-to-backend";
 
 i18n
-  .use(i18nBackend)
+  .use(
+    resourcesToBackend((language, namespace, callback) => {
+      import(`./i18n/${language}.json`)
+        .then((resources) => {
+          callback(null, resources);
+        })
+        .catch((error) => {
+          callback(error, null);
+        });
+    })
+  )
   .use(initReactI18next)
   .init({
     lng: "en",
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
     },
   });
 
